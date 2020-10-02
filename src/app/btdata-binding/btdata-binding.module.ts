4dbd299921b16ad4e409ab758e68cd6551637f46
phthaohuong <thaohuongtqt1603@gmail.com>
2020-10-02 12:09:05 +0700

    Add_BT_DataBinding

--- src/app/btdata-binding/btdata-binding.module.ts
+++ src/app/btdata-binding/btdata-binding.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BTDataBindingComponent } from "./btdata-binding/btdata-binding.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [BTDataBindingComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [BTDataBindingComponent],
 })
 export class BTDataBindingModule {}
--- src/app/btdata-binding/btdata-binding/btdata-binding.component.ts
+++ src/app/btdata-binding/btdata-binding/btdata-binding.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-btdata-binding',
-  templateUrl: './btdata-binding.component.html',
-  styleUrls: ['./btdata-binding.component.scss']
+  selector: "app-btdata-binding",
+  templateUrl: "./btdata-binding.component.html",
+  styleUrls: ["./btdata-binding.component.scss"],
 })
 export class BTDataBindingComponent implements OnInit {
+  user: any = {
+    email: "Enter Your Email",
+    name: "Enter Your Name",
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  submitEmail(inputEmail) {
+    this.user.email = inputEmail.value;
   }
-
 }
